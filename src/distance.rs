c19504f2d6cbc4153d9eca4aaf7ecd2928afa737
pjottos <pjotrslooff@icloud.com>
2020-01-01 15:33:34 +0100

    distance sensor

---
+++ src/distance.rs
@@ -0,0 +1,56 @@
+use gpio_cdev::{Chip, LineRequestFlags, EventRequestFlags, EventType, Line, LineHandle};
+use std::{thread, time};
+
+const SOUND_SPEED: f64 = 343.0;
+
+pub struct UltrasonicSensor {
+    trig: LineHandle,
+    echo: Line,
+}
+
+impl UltrasonicSensor {
+    pub fn new(trig_pin: u32, echo_pin: u32) -> gpio_cdev::errors::Result<UltrasonicSensor> {
+        let mut chip = Chip::new("/dev/gpiochip0")?;
+        
+        let echo = chip.get_line(echo_pin)?;
+        let trig = chip.get_line(trig_pin)?.request(LineRequestFlags::OUTPUT, 0, "ultrasonic")?;
+
+        Ok(UltrasonicSensor {
+            echo: echo,
+            trig: trig,
+        })
+    }
+
+    fn trigger(&self) -> gpio_cdev::errors::Result<()> {
+        self.trig.set_value(1)?;
+        thread::sleep(time::Duration::from_nanos(10));
+        self.trig.set_value(0)?;
+
+        Ok(())
+    }
+
+    pub fn poll(&self) -> gpio_cdev::errors::Result<f64> {
+        let mut last_echo_time = time::Instant::now();
+        
+        self.trigger()?;
+    
+        for event in self.echo.events(
+            LineRequestFlags::INPUT,
+            EventRequestFlags::BOTH_EDGES,
+            "ultrasonic",
+        )? {
+            let evt = event?;
+            match evt.event_type() {
+                EventType::RisingEdge => {
+                    last_echo_time = time::Instant::now();
+                }
+                EventType::FallingEdge => {
+                    let t = last_echo_time.elapsed().as_nanos();
+                    return Ok((t as f64 / 1000000000.0) * 0.5 * SOUND_SPEED);
+                }
+            }
+        }
+    
+        panic!("ultrasonic event loop stopped")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,78 +8,36 @@ use opencv::{
     videoio,
 };
 
+mod distance;
+use self::distance::UltrasonicSensor;
+
+const PIN_TRIG: u32 = 14;
+const PIN_ECHO: u32 = 15;
+
 //use seperate function for convenience with opencv result handling
 fn run() -> opencv::Result<()> {
-    let window_title = "test";
-    highgui::named_window(window_title, 1)?;
+    let left_title = "left";
+    let right_title = "right";
 
-    let face_config = core::find_file("haarcascades/haarcascade_frontalface_alt.xml", true, false)?;
-    let mut cam = videoio::VideoCapture::new_with_backend(0, videoio::CAP_ANY)?;
+    highgui::named_window(left_title, 1)?;
+    highgui::named_window(right_title, 1)?;
 
-    if !videoio::VideoCapture::is_opened(&cam)? {
-        panic!("could not open camera");
+    let mut left = videoio::VideoCapture::new_with_backend(0, videoio::CAP_ANY)?;
+    let mut right = videoio::VideoCapture::new_with_backend(4, videoio::CAP_ANY)?;
+
+    if !(videoio::VideoCapture::is_opened(&left)? && videoio::VideoCapture::is_opened(&right)?) {
+        panic!("could not open cameras");
     }
     
-    let mut detector = objdetect::CascadeClassifier::new(&face_config)?;
-    
     loop {
-        let mut frame = Mat::default()?;
+        let mut left_view = Mat::default()?;
+        left.read(&mut left_view)?;
 
-        cam.read(&mut frame)?;
+        let mut right_view = Mat::default()?;
+        right.read(&mut right_view)?;
 
-        //make grayscale
-        let mut gray = Mat::default()?;
-        imgproc::cvt_color(
-            &frame,
-            &mut gray,
-            imgproc::COLOR_BGR2GRAY,
-            0
-        )?;
-
-        //use lower res for faster processing
-        // let mut reduced = Mat::default()?;
-        // imgproc::resize(
-        //     &gray,
-        //     &mut reduced,
-        //     core::Size {
-        //         width: 0,
-        //         height: 0
-        //     },
-        //     0.25f64,
-        //     0.25f64,
-        //     imgproc::INTER_LINEAR
-        // )?;
-
-        let mut faces = types::VectorOfRect::new();
-        detector.detect_multi_scale(
-            &gray,
-            &mut faces,
-            1.1,
-            2,
-            objdetect::CASCADE_SCALE_IMAGE,
-            core::Size {
-                width: 30,
-                height: 30
-            },
-            core::Size {
-                width: 0,
-                height: 0
-            }
-        )?;
-        println!("found {} faces", faces.len());
-        for face in faces {
-            //draw rectangle around every face
-            imgproc::rectangle(
-                &mut frame,
-                face,
-                core::Scalar::new(0f64, -1f64, -1f64, -1f64),
-                1,
-                8,
-                0
-            )?;
-        }
-
-        highgui::imshow(window_title, &frame)?;
+        highgui::imshow(left_title, &left_view)?;
+        highgui::imshow(right_title, &right_view)?;
 
         if highgui::wait_key(10)? > 0 { //esc key
             break;
@@ -89,5 +47,7 @@ fn run() -> opencv::Result<()> {
 }
 
 fn main() {
-    run().unwrap()
+    let dist = UltrasonicSensor::new(PIN_TRIG, PIN_ECHO).unwrap();
+    println!("test distance: {:.0}", dist.poll().unwrap());
+    //run().unwrap()
 }
